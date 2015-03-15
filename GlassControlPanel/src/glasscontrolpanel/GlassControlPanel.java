15d5a7fda4be04ffa725bdf2dd7283ee6aed38b8
Taliesin0 <jwhite9@jwhite9_win8.gonzaga.edu>
2015-03-15 15:20:49 -0700

    Merge branch 'master' of https://github.com/GliderWinchCommons/cp

--- GlassControlPanel/src/glasscontrolpanel/GlassControlPanel.java
+++ GlassControlPanel/src/glasscontrolpanel/GlassControlPanel.java
@@ -19,19 +19,20 @@ public class GlassControlPanel
     ////////////////////////////////////////////////
     static final int ID_OFFSET = 1 << 21;
     ////////////////////////////////////////////////
-    static final int TIME_MESSAGE_ID = 256 * ID_OFFSET;             // 0x200
-    static final int MOTOR_MESSAGE_ID = 296 * ID_OFFSET;            // 0x250
-    static final int TORQUE_MESSAGE_ID = 300 * ID_OFFSET;           // 0x258
-    static final int STATE_MESSAGE_ID = 304 * ID_OFFSET;            // 0x260
-    static final int LAUNCH_PARAM_MESSAGE_ID = 327 * ID_OFFSET;     // 0x28e
-    static final int TENSION_MESSAGE_ID = 448 * ID_OFFSET;          // 0x380
-    static final int CABLE_ANGLE_MESSAGE_ID = 464 * ID_OFFSET;      // 0x3a0
-    static final int PARAM_REQUEST_MESSAGE_ID = 312 * ID_OFFSET;    // 0x270    
-    static final int CP_CONTROL_LEVER_RMT = 328 * ID_OFFSET;        // 0x290
-    static final int CP_CONTROL_LEVER_LCL = 329 * ID_OFFSET;        // 0x292
-    static final int CP_CONTROL_LEVER_INPUTS_RMT = 330 * ID_OFFSET; // 0x294
-    static final int CP_CONTROL_LEVER_INPUTS_LCL = 331 * ID_OFFSET; // 0x296
-    static final int CONTROL_LEVER_MESSAGE_ID = 641 * ID_OFFSET;    // 0x502
+    static final int TIME_MESSAGE_ID             = 256 * ID_OFFSET;    // 0x200
+    static final int MOTOR_MESSAGE_ID            = 296 * ID_OFFSET;    // 0x250
+    static final int TORQUE_MESSAGE_ID           = 300 * ID_OFFSET;    // 0x258
+    static final int STATE_MESSAGE_ID            = 304 * ID_OFFSET;    // 0x260
+    static final int LAUNCH_PARAM_MESSAGE_ID     = 327 * ID_OFFSET;    // 0x28e
+    static final int TENSION_MESSAGE_ID          = 448 * ID_OFFSET;    // 0x380
+    static final int CABLE_ANGLE_MESSAGE_ID      = 464 * ID_OFFSET;    // 0x3a0
+    static final int PARAM_REQUEST_MESSAGE_ID    = 312 * ID_OFFSET;    // 0x270    
+    static final int CP_CONTROL_LEVER_RMT        = 328 * ID_OFFSET;    // 0x290
+    static final int CP_CONTROL_LEVER_LCL        = 329 * ID_OFFSET;    // 0x292
+    static final int CP_CONTROL_LEVER_INPUTS_RMT = 330 * ID_OFFSET;    // 0x294
+    static final int CP_CONTROL_LEVER_INPUTS_LCL = 331 * ID_OFFSET;    // 0x296
+    static final int CONTROL_LEVER_MESSAGE_ID    = 641 * ID_OFFSET;    // 0x502
+    static final int HEARTBEAT_COUNT_ID           = 60;                 //%d only
     ////////////////////////////////////////////////
     static final String HUBSERVER_ADDRESS = "147.222.165.75";       //HUB-SERVER
     static final int HUBSERVER_PORT = 32123;
@@ -54,10 +55,14 @@ public class GlassControlPanel
 
         CanCnvt canIn = new CanCnvt();
         String msg;
-  
+
         ControlPanel cp = new ControlPanel();
         cp.setVisible(true);
 
+        float current_lever_position = cp.getSlider();
+        int current_state = cp.getState();
+        int lastMessage;
+        
         try
         {            
             Socket connection = new Socket(HUBSERVER_ADDRESS, HUBSERVER_PORT);
@@ -65,7 +70,7 @@ public class GlassControlPanel
 
             BufferedReader instream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             OutputStreamWriter outstream = new OutputStreamWriter(connection.getOutputStream());
-
+            int count = 0;
             while (true)
             {
                 if ((msg = instream.readLine()) == null)
@@ -74,16 +79,28 @@ public class GlassControlPanel
                 } 
                 else
                 {
-                    canIn.convert_msgtobin(msg);
-
-                    switch (canIn.id)
+                    if( HEARTBEAT_COUNT_ID - count == 0 ||
+                        cp.getState() != current_state ||
+                        cp.getSlider() != current_lever_position)
                     {
-                        case TIME_MESSAGE_ID:
-                            gcpMessageControlLever.set_halffloat(cp.getSlider(), 0);
-                            gcpMessageInputs.set_short(cp.getGCPInteraction(), 0);
-                            outstream.write(gcpMessageControlLever.msg_prep());
-                            outstream.write(gcpMessageInputs.msg_prep());
-                            outstream.flush();
+                        canIn.convert_msgtobin(msg);
+
+                        switch (canIn.id)
+                        {
+                            case TIME_MESSAGE_ID:
+                                gcpMessageControlLever.set_halffloat(cp.getSlider(), 0);
+                                gcpMessageInputs.set_short(cp.getGCPInteraction(), 0);
+                                outstream.write(gcpMessageControlLever.msg_prep());
+                                outstream.write(gcpMessageInputs.msg_prep());
+                                outstream.flush();
+                        }
+                        count = 0;
+                        current_state = cp.getState();
+                        current_lever_position = cp.getSlider();
+                    }
+                    else
+                    {
+                        count++;
                     }
                 }
             }
